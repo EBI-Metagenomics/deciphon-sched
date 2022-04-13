ea20b6d1f8f9cd72506ce1eed4b727a17e6ba260
Danilo Horta <danilo.horta@pm.me>
2022-04-13 15:13:29 +0100

    NUM_SEQS_PER_JOB=512

--- include/sched/limits.h
+++ include/sched/limits.h
@@ -9,7 +9,7 @@ enum sched_limits
     JOB_STATE_SIZE = 5,
     MATCH_SIZE = 5 * (1024 * 1024),
     MAX_NUM_THREADS = 64,
-    NUM_SEQS_PER_JOB = 64,
+    NUM_SEQS_PER_JOB = 512,
     PATH_SIZE = 4096,
     PROFILE_NAME_SIZE = 64,
     PROFILE_TYPEID_SIZE = 16,
