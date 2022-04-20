98ae148d8298d7fc0402f4b8cb47ccfa416f25c5
Danilo Horta <danilo.horta@pm.me>
2022-04-20 11:11:13 +0100

    remove struct sqlite3, logger from export

--- include/sched/sched.h
+++ include/sched/sched.h
@@ -2,16 +2,14 @@
 #define SCHED_SCHED_H
 
 #include "sched/db.h"
+#include "sched/error.h"
 #include "sched/hmm.h"
 #include "sched/job.h"
 #include "sched/limits.h"
-#include "sched/logger.h"
 #include "sched/prod.h"
 #include "sched/rc.h"
 #include "sched/scan.h"
 #include "sched/seq.h"
-
-struct sqlite3;
 
 struct sched_health
 {
